d19ea594a864eff0d43e46cb00815c76ce5545f1
Jérémy Marmol <jeremy.marmol@fr.clara.net>
2019-10-29 16:09:24 +0100

    AZ-94: Upgrade module to tf0.12

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals {
   default_tags = {
-    env   = "${var.environment}"
-    stack = "${var.stack}"
+    env   = var.environment
+    stack = var.stack
   }
 
   container_default_image = {
@@ -10,18 +10,38 @@ locals {
     dotnet = "microsoft/azure-functions-dotnet-core2.0:2.0"
   }
 
-  name_prefix          = "${var.name_prefix != "" ? format("%s-", var.name_prefix) : ""}"
-  ai_name_prefix       = "${var.application_insights_name_prefix != "" ? format("%s-", var.application_insights_name_prefix) : local.name_prefix}"
-  function_name_prefix = "${var.function_app_name_prefix != "" ? format("%s-", var.function_app_name_prefix) : local.name_prefix}"
+  name_prefix          = var.name_prefix != "" ? format("%s-", var.name_prefix) : ""
+  ai_name_prefix       = var.application_insights_name_prefix != "" ? format("%s-", var.application_insights_name_prefix) : local.name_prefix
+  function_name_prefix = var.function_app_name_prefix != "" ? format("%s-", var.function_app_name_prefix) : local.name_prefix
 
-  storage_default_name_long         = "${replace(format("%s%s%s", coalesce(var.storage_account_name_prefix, var.name_prefix), var.stack, var.environment), "/[._-]/", "")}"
-  storage_default_name              = "${substr(local.storage_default_name_long, 0, length(local.storage_default_name_long) > 24 ? 23 : -1)}"
-  storage_account_connection_string = "${var.create_storage_account_resource == "true" ? join("", azurerm_storage_account.storage.*.primary_connection_string) : var.storage_account_connection_string}"
+  storage_default_name_long = replace(
+    format(
+      "%s%s%s",
+      coalesce(var.storage_account_name_prefix, var.name_prefix),
+      var.stack,
+      var.environment,
+    ),
+    "/[._-]/",
+    "",
+  )
+  storage_default_name = substr(
+    local.storage_default_name_long,
+    0,
+    length(local.storage_default_name_long) > 24 ? 23 : -1,
+  )
+  storage_account_connection_string = var.create_storage_account_resource == "true" ? join(
+    "",
+    azurerm_storage_account.storage.*.primary_connection_string,
+  ) : var.storage_account_connection_string
 
-  app_insights_instrumentation_key = "${var.create_application_insights_resource == "true" ? join("", azurerm_application_insights.app_insights.*.instrumentation_key) : var.application_insights_instrumentation_key}"
+  app_insights_instrumentation_key = var.create_application_insights_resource == "true" ? join(
+    "",
+    azurerm_application_insights.app_insights.*.instrumentation_key,
+  ) : var.application_insights_instrumentation_key
 
   default_application_settings = {
-    FUNCTIONS_WORKER_RUNTIME       = "${var.function_language_for_linux}"
-    APPINSIGHTS_INSTRUMENTATIONKEY = "${local.app_insights_instrumentation_key}"
+    FUNCTIONS_WORKER_RUNTIME       = var.function_language_for_linux
+    APPINSIGHTS_INSTRUMENTATIONKEY = local.app_insights_instrumentation_key
   }
 }
+
