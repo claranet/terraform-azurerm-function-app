fe215530cf1221d27b09da7ace0d8d96204cff00
Jérémy MARMOL <jeremy.marmol@fr.clara.net>
2020-07-22 16:11:53 +0200

    AZ-198: use coalesce instead of condition

--- resources.tf
+++ resources.tf
@@ -6,7 +6,7 @@ data "azurerm_app_service_plan" "plan" {
 
 # Storage account
 resource "azurerm_storage_account" "storage" {
-  name = var.storage_account_name == null ? local.storage_default_name : var.storage_account_name
+  name = coalesce(var.storage_account_name, local.storage_default_name)
 
   location            = var.location
   resource_group_name = var.resource_group_name
