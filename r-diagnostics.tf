58e395284e9f400b5430fdac96783c8cbfe5808d
Laurent Piroelle <laurent.piroelle@fr.clara.net>
2020-11-27 09:13:46 +0100

    AZ-366: update diagnostics settings reference

--- r-diagnostics.tf
+++ r-diagnostics.tf
@@ -1,11 +1,10 @@
 module "diagnostics" {
-  count  = var.logs_destinations_ids != [] ? 1 : 0
-  source = "git::ssh://git@git.fr.clara.net/claranet/projects/cloud/azure/terraform/modules/diagnostic-settings.git?ref=AZ-363-log-analytics-destination-type"
+  source  = "claranet/diagnostic-settings/azurerm"
+  version = "4.0.0"
 
   resource_id           = azurerm_function_app.function_app.id
   logs_destinations_ids = var.logs_destinations_ids
   log_categories        = var.logs_logs_categories
   metric_categories     = var.logs_metrics_categories
   retention_days        = var.log_retention_days
-
 }
