01f6b2da691b8a6bd90034d5b10c37380f289069
Jérôme Respaut <jerome.respaut@fr.clara.net>
2021-12-07 23:58:09 +0100

    AZ-515: tflint ignore errors

--- modules/functionapp/locals-naming.tf
+++ modules/functionapp/locals-naming.tf
@@ -3,11 +3,11 @@ locals {
   name_suffix = lower(var.name_suffix)
   name_prefix = lower(var.name_prefix)
 
-  function_name_prefix = coalesce(var.function_app_name_prefix, local.name_prefix)
-  ai_name_prefix       = coalesce(var.application_insights_name_prefix, local.name_prefix)
-  sa_name_prefix       = coalesce(var.storage_account_name_prefix, local.name_prefix)
+  function_name_prefix = var.function_app_name_prefix == "" ? local.name_prefix : var.function_app_name_prefix
+  ai_name_prefix       = var.application_insights_name_prefix == "" ? local.name_prefix : var.application_insights_name_prefix
+  sa_name_prefix       = var.storage_account_name_prefix == "" ? local.name_prefix : var.storage_account_name_prefix
 
-  app_insights_name    = coalesce(var.application_insights_custom_name, azurecaf_name.application_insights.name)
-  function_app_name    = coalesce(var.function_app_custom_name, azurecaf_name.function_app.name)
-  storage_account_name = coalesce(var.storage_account_name, azurecaf_name.storage_account.name)
+  app_insights_name    = coalesce(var.application_insights_custom_name, azurecaf_name.application_insights.result)
+  function_app_name    = coalesce(var.function_app_custom_name, azurecaf_name.function_app.result)
+  storage_account_name = coalesce(var.storage_account_name, azurecaf_name.storage_account.result)
 }
--- versions.tf
+++ versions.tf
@@ -1,11 +1,14 @@
 terraform {
   required_version = ">= 0.13"
 
+  # https://github.com/terraform-linters/tflint/blob/v0.33.2/docs/rules/terraform_unused_required_providers.md
   required_providers {
+    # tflint-ignore: terraform_unused_required_providers
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 2.57"
     }
+    # tflint-ignore: terraform_unused_required_providers
     azurecaf = {
       source  = "aztfmod/azurecaf"
       version = "~> 1.1"
