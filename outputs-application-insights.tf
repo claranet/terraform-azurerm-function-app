d0f1fdda81c48fabac2b389f13e78f3321018c10
Sébastien Caloone <sebastien.caloone@fr.clara.net>
2022-06-15 10:08:35 +0200

    AZ-776 Fix outputs ressource address

--- outputs-application-insights.tf
+++ outputs-application-insights.tf
@@ -1,25 +1,25 @@
 output "application_insights_id" {
   description = "ID of the associated Application Insights"
-  value       = module.linux_function.application_insights_id
+  value       = local.function_output.application_insights_id
 }
 
 output "application_insights_name" {
   description = "Name of the associated Application Insights"
-  value       = module.linux_function.application_insights_name
+  value       = local.function_output.application_insights_name
 }
 
 output "application_insights_app_id" {
   description = "App ID of the associated Application Insights"
-  value       = module.linux_function.application_insights_app_id
+  value       = local.function_output.application_insights_app_id
 }
 
 output "application_insights_instrumentation_key" {
   description = "Instrumentation key of the associated Application Insights"
-  value       = module.linux_function.application_insights_instrumentation_key
+  value       = local.function_output.application_insights_instrumentation_key
   sensitive   = true
 }
 
 output "application_insights_application_type" {
   description = "Application Type of the associated Application Insights"
-  value       = module.linux_function.application_insights_application_type
+  value       = local.function_output.application_insights_application_type
 }
