42c3cbaee4e8ae218c96c1d7380e9cfd4ea8111b
Jérôme Respaut <jerome.respaut@fr.clara.net>
2022-08-05 17:49:13 +0200

    AZ-130: Explicit `data_protection` options

--- modules/linux-function/r-storage.tf
+++ modules/linux-function/r-storage.tf
@@ -28,7 +28,13 @@ module "storage" {
   identity_ids  = var.storage_account_identity_ids
 
   # Data protection - not needed for functions
-  storage_blob_data_protection = null
+  storage_blob_data_protection = {
+    change_feed_enabled                       = false
+    versioning_enabled                        = false
+    delete_retention_policy_in_days           = 0
+    container_delete_retention_policy_in_days = 0
+    container_point_in_time_restore           = false
+  }
 
   # Network rules - handle out of module to avoid Terraform cycle
   network_rules_enabled = false
