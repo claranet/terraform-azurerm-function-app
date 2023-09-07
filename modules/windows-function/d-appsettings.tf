9eaec8ea4ff6e111a3ffaf991a74838de1c3eae5
Jérémy MARMOL <jeremy.marmol@fr.clara.net>
2023-09-07 16:32:17 +0200

    AZ-1133: Fix script path for windows functions

--- modules/windows-function/d-appsettings.tf
+++ modules/windows-function/d-appsettings.tf
@@ -2,7 +2,7 @@ data "azurerm_subscription" "current" {}
 
 data "external" "app_service_settings" {
   count   = var.function_app_application_settings_drift_ignore ? 1 : 0
-  program = ["sh", format("%s/../files/webapp_setting.sh", path.module)]
+  program = ["sh", format("%s/../../files/webapp_setting.sh", path.module)]
   query = {
     webapp_name  = local.function_app_name
     rg_name      = var.resource_group_name
